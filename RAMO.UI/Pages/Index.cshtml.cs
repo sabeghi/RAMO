b8d44ca8b796b0aaa7a8c39ec2b293792ab342d7
Mostafa Sabeghi <34593845+sabeghi@users.noreply.github.com>
2023-09-13 11:05:28 +0330

    init proj

---
+++ RAMO.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RAMO.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public static string OnGet()
+        {
+            return "<div>salam</div>";
+        }
+    }
+}
